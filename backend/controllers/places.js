623569c047775f038f47a470397b12b7fff60f55
CyanFaux <santafox26@gmail.com>
2024-04-27 20:45:26 -0400

    holy cow it actually works

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -13,6 +13,9 @@ router.post('/', async (req, res) => {
     if (!req.body.state) {
         req.body.state = 'USA'
     }
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to create a place' })
+    }
     const place = await Place.create(req.body)
     res.json(place)
 })
@@ -60,6 +63,9 @@ router.put('/:placeId', async (req, res) => {
             res.json(place)
         }
     }
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to edit places' })
+    }
 })
 
 router.delete('/:placeId', async (req, res) => {
@@ -78,6 +84,9 @@ router.delete('/:placeId', async (req, res) => {
             await place.destroy()
             res.json(place)
         }
+    }
+    if (req.currentUser?.role !== 'admin') {
+        return res.status(403).json({ message: 'You are not allowed to delete places' })
     }
 })
 
